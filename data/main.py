9553d3925d62eb6402ce77bb9ea7c2f5bc57242e
Conner Smith <connerpsmith@gmail.com>
2024-06-19 17:29:02 -0500

    production method parsing

--- data/main.py
+++ data/main.py
@@ -2,6 +2,7 @@ from buildings_parser import BuildingsParser
 from goods_parser import GoodsParser
 from production_method_groups_parser import ProductionMethodGroupsParser
 from pop_types_parser import PopTypesParser
+from production_methods_parser import ProductionMethodsParser
 
 def main():
     building_parser = BuildingsParser('./buildings')
@@ -12,12 +13,16 @@ def main():
     result = goods_parser.parse()
     print(f"Output: {result}")
 
-    goods_parser = GoodsParser('./goods')
-    result = goods_parser.parse()
+    pop_types_parser = PopTypesParser('./pop_types')
+    result = pop_types_parser.parse()
     print(f"Output: {result}")
 
     production_method_groups_parser = ProductionMethodGroupsParser('./production_method_groups')
     result = production_method_groups_parser.parse()
+    print(f"Output: {result}")
+
+    production_method_parser = ProductionMethodsParser('./production_methods')
+    result = production_method_parser.parse()
     print(f"Output: {result}")
 
 main()
--- data/production_methods_parser.py
+++ data/production_methods_parser.py
@@ -6,6 +6,9 @@ class ProductionMethodsParser:
     def __init__(self, folder_path):
         self.folder_path = folder_path
         self.is_unlocking_technologies_open = False
+        self.is_building_modifiers_open = False
+        self.is_workforce_scaled_open = False
+        self.is_level_scaled_open = False
 
     def parse(self):
         item_list = []
@@ -19,13 +22,14 @@ class ProductionMethodsParser:
                     item_list += cleaner.clean(self.callback)
 
                     file.close()
-        with open(os.path.join(self.folder_path, 'buildings.json'), 'w') as json_file:
+        with open(os.path.join(self.folder_path, 'production_methods.json'), 'w') as json_file:
             json.dump(item_list, json_file)
 
         return item_list
 
     def callback(self, line, item_dict):
         item_dict = self.handleUnlockingTechnologies(line, item_dict)
+        item_dict = self.handleBuildingModifiers(line, item_dict)
         return item_dict
 
     def handleUnlockingTechnologies(self, line, item_dict):
@@ -41,3 +45,53 @@ class ProductionMethodsParser:
                 item_dict["unlocking_technologies"].append(line)
 
         return item_dict
+
+    def handleBuildingModifiers(self, line, item_dict):
+        isHeaderInLine = "building_modifiers" in line
+        if isHeaderInLine and self.is_building_modifiers_open == False:
+            self.is_building_modifiers_open = True
+
+        if self.is_building_modifiers_open:
+            self.handleWorkforceScaled(line, item_dict)
+
+        if self.is_building_modifiers_open and self.is_workforce_scaled_open == False and isHeaderInLine != True:
+            if "}" in line:
+                self.is_building_modifiers_open = False
+
+        return item_dict
+
+    def handleWorkforceScaled(self, line, item_dict):
+        isHeaderInLine = "workforce_scaled" in line
+        if isHeaderInLine and self.is_workforce_scaled_open == False:
+            item_dict["inputs"] = []
+            item_dict["outputs"] = []
+            self.is_workforce_scaled_open = True
+
+        if self.is_workforce_scaled_open and isHeaderInLine != True:
+            if "}" in line:
+                self.is_workforce_scaled_open = False
+            else:
+                if(line.strip() != ""):
+                    goods = self.open_goods_json()
+
+                    if("input" in line and "#" not in line):
+                        results = [good for good in goods if good["name"] in line]
+                        input_item = {
+                            "good": results[0]["name"],
+                            "amount": line.split("=")[1].strip()
+                        }
+                        item_dict["inputs"].append(input_item)
+                    elif("output" in line and "#" not in line):
+                        results = [good for good in goods if good["name"] in line]
+                        output_item = {
+                            "good": results[0]["name"],
+                            "amount": line.split("=")[1].strip()
+                        }
+                        item_dict["outputs"].append(output_item)
+
+        return item_dict
+
+    def open_goods_json(self):
+        with open('./goods/goods.json', 'r') as json_file:
+            data = json.load(json_file)
+        return data
